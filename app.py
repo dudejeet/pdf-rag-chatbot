a8a9a88c0af388a89162a2e54a8e1a57c736d6d6
Amanjeet Singh <142820369+dudejeet@users.noreply.github.com>
2026-06-01 20:06:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain.chains import RetrievalQA
+#from langchain.chains import RetrievalQA
 
 load_dotenv()
 
